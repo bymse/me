1bf12e7711f22aec12a1ce55a0b2fae031f4240f
bymse <egorstrelov@gmail.com>
2023-12-10 20:30:27 +0300

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+  return {
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+    // These are all optional:
+    dir: {
+      input: "content", // default: "."
+      includes: "../_includes", // default: "_includes"
+      data: "../_data", // default: "_data"
+      output: "_site",
+    },
+  };
+};
