6e172c0973b0147b385677cc115926897048a47f
bymse <egorstrelov@gmail.com>
2024-02-04 21:31:38 +0300

    Minify and inline css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,14 @@
+const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
   eleventyConfig.addFilter("jsonify", function (variable) {
     return JSON.stringify(variable);
+  });
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 
   return {
